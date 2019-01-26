79b4dea01517ad1a99dcd685d039c171db7851d9
Musa <tamzid101@gmail.com>
2019-01-26 21:36:21 +0600

    trainer is creating model.

---
+++ trainer.py
@@ -0,0 +1,88 @@
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+
+class Trainer:
+
+    def __init__(self , train_dir , test_dir , validation_dir):
+
+        self.train_dir = train_dir
+        self.test_dir = test_dir
+        self.validation_dir =validation_dir
+
+    def _init_model(self):
+
+        self.model = models.Sequential()
+
+        self.model.add(layers.Conv2D(32 , (3 , 3) , activation = "relu" , input_shape = ( 150 , 150 , 3 )))
+        self.model.add(layers.MaxPooling2D((2 , 2)))
+
+
+        self.model.add(layers.Conv2D(64, (3 , 3) , activation = "relu" ))
+        self.model.add(layers.MaxPooling2D((2 , 2)))
+
+
+        self.model.add(layers.Conv2D(128 , (3 , 3) , activation = "relu" ))
+        self.model.add(layers.MaxPooling2D((2 , 2)))
+
+
+        self.model.add(layers.Conv2D(128 , (3 , 3) , activation = "relu" ))
+        self.model.add(layers.MaxPooling2D((2 , 2)))
+
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(512 , activation = "relu"))
+        self.model.add(layers.Dense(1 , activation = "sigmoid"))
+
+        self.model.summary()
+        self.model.compile(loss = "binary_crossentropy" ,
+                optimizer = optimizers.RMSprop(lr = 1e-4) ,
+                metrics = ["acc"])
+
+
+    def _init_data(self):
+
+        train_datagen = ImageDataGenerator(
+                rescale = 1. / 255 ,
+                rotation_range = 40 ,
+                width_shift_range = 0.2 ,
+                height_shift_range = 0.2 ,
+                shear_range = 0.2 ,
+                zoom_range = 0.2 ,
+                horizontal_flip = True
+                )
+
+        test_datagen = ImageDataGenerator(rescale = 1. / 255)
+
+
+        self.train_generator = train_datagen.flow_from_directory(
+                self.train_dir,
+                target_size = (150 , 150),
+                batch_size = 32 ,
+                class_mode = "binary"
+                )
+
+
+        self.validation_generator = test_datagen.flow_from_directory(
+                self.validation_dir,
+                target_size = (150 , 150),
+                batch_size = 32 ,
+                class_mode = "binary"
+                )
+
+    def train(self):
+
+        self._init_model()
+        self._init_data()
+
+
+
+def main():
+
+    trainer = Trainer(train_dir = "./train" , test_dir = "./test" , validation_dir = "./validation")
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
